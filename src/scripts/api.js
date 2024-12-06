4ad50d1ac44bea20746cc59f2d5934251b0628e9
Heaven Aldrico <4d3.h34v3n@gmail.com>
2024-12-07 04:11:51 +0700

    done

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,19 +1,19 @@
 // TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'YOUR_BASE_URL';
+const BASE_URL = "https://ml-api-591680438085.asia-southeast2.run.app";
 
 const ENDPOINT = {
-  predict: `${BASE_URL}/predict`,
+    predict: `${BASE_URL}/predict`,
 };
 
 class PredictAPI {
-  static async predict(data) {
-    const response = await fetch(ENDPOINT.predict, {
-      method: 'POST',
-      body: data,
-      redirect: 'follow',
-    });
+    static async predict(data) {
+        const response = await fetch(ENDPOINT.predict, {
+            method: "POST",
+            body: data,
+            redirect: "follow",
+        });
 
-    const json = await response.json();
-    return json;
-  }
+        const json = await response.json();
+        return json;
+    }
 }
